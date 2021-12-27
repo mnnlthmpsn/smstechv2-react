24c95c1d43d9eca762507eb1fba3b22181f1784b
unknown <mnnlthmpsn@outlook.com>
2021-12-27 00:13:22 -0800

    fix api request path

--- src/api/base.js
+++ src/api/base.js
@@ -1,3 +1,3 @@
-const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:1337/api' : 'https://portal-smstech.herokuapp.com/'
+const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:1337/api' : 'https://portal-smstech.herokuapp.com'
 
 export default BASE_URL
