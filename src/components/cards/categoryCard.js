4b7ece01140e3a7310cc6e57b0e91b2ce096ff86
unknown <mnnlthmpsn@outlook.com>
2022-01-23 19:42:51 -0800

    mb-3

--- src/components/cards/categoryCard.js
+++ src/components/cards/categoryCard.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const CategoryCard = ({ category }) => {
 
     return (
-        <div className="product product-7 text-center">
+        <div className="product product-7 text-center mb-3">
             <figure className="product-media">
                 <Link to={`${category.attributes.slug}`} state={{ categoryName: category.attributes.title }}>
                     <img src={category.attributes.thumbnail.data.attributes.url} alt={category.attributes.title} className="product-image" loading="lazy"/>
