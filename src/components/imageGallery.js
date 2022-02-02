350f9be059c4bf85c2e66e36a4dfb382832a90ec
unknown <mnnlthmpsn@outlook.com>
2022-02-02 11:49:56 -0800

    image gallery ui fix

--- src/components/imageGallery.js
+++ src/components/imageGallery.js
@@ -8,15 +8,14 @@ const ImageGallery = ({ images }) => {
         <div className="product-gallery product-gallery-vertical">
             <div className="row">
                 <figure className="product-main-image">
-                    <img id="product-zoom" src={active} data-zoom-image={active} alt="product image" />
-
+                    <img className="selected_image" id="product-zoom" src={active} data-zoom-image={active} alt="product image" />
                 </figure>
 
                 <div id="product-zoom-gallery" className="product-image-gallery">
                     {
                         images.map(image => (
                             <span onClick={() => setActive(image.attributes.url)} className={`product-gallery-item cursor ${image.attributes.url === active ? 'active' : undefined}`} data-image="/assets/images/products/single/1.jpg" data-zoom-image="/assets/images/products/single/1-big.jpg" key={image.id}>
-                                <img src={image.attributes.url} alt="product side" />
+                                <img className="image_thumbnail" src={image.attributes.url} alt="product side" />
                             </span>
                         ))
                     }
